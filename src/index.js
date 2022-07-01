79568067b082d192f6a6053b4a125aaea2367f31
Artur Abdullin <artur.abdullin@gmail.com>
2022-07-01 13:55:42 +0300

    feat: complete function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+/**
+ *
+ * @param {string} s
+ * @param {string[][]} bracketsConfig
+ * @returns {boolean}
+ */
+function check(s, bracketsConfig) {
+  let map = new Map();
+  for (let conf of bracketsConfig) {
+    map.set(conf[1], conf[0]);
+  }
+
+  let tempArray = [];
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i]) && tempArray[tempArray.length - 1] === map.get(s[i])) {
+      tempArray.pop();
+      continue;
+    }
+    tempArray.push(s[i]);
+  }
+
+  return tempArray.length === 0;
 }
+
+module.exports = check;
